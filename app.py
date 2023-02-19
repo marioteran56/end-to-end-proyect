317a51cd4248c09b5d9d56c982cba984d8323fd4
Mario Alberto Terán Acosta <marioteran1@hotmail.com>
2023-02-18 23:07:32 -0600

    Module addition

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from prediction import predict
-from sklearn.compose import ColumnTransformer
+from combined_attributes_adder import CombinedAttributesAdder
 
 st.title('Prediciendo el precio de una casa en California')
 st.markdown('Multiples modelos para la predicción de precios de casas en California...')
@@ -12,14 +12,14 @@ col1, col2 = st.columns(2)
 with col1:
     longitude = st.number_input('Longitud', value=-122.23)
     latitude = st.number_input('Latitud', value=37.88)
-    housing_median_age = st.number_input('Edad media de la vivienda', value=28)
-    total_rooms = st.number_input('Total de habitaciones', value=1000)
-    total_bedrooms = st.number_input('Total de habitaciones', value=200)
+    housing_median_age = st.number_input('Edad media de la vivienda', value=41)
+    total_rooms = st.number_input('Total de habitaciones', value=880)
+    total_bedrooms = st.number_input('Total de habitaciones', value=129)
 with col2:
-    population = st.number_input('Población', value=1000)
-    households = st.number_input('Casas', value=200)
-    median_income = st.number_input('Ingreso medio', value=3)
-    ocean_proximity = st.selectbox('Proximidad al océano', ['<1H OCEAN', 'INLAND', 'ISLAND', 'NEAR BAY', 'NEAR OCEAN'])
+    population = st.number_input('Población', value=322)
+    households = st.number_input('Casas', value=126)
+    median_income = st.number_input('Ingreso medio', value=8.33)
+    ocean_proximity = st.selectbox('Proximidad al océano', ['<1H OCEAN', 'INLAND', 'ISLAND', 'NEAR BAY', 'NEAR OCEAN'], index=3)
     
 st.header('Modelos')
 model = st.selectbox('Modelo', ['Linear Regression', 'Random Forest Regressor', 'Decision Tree Regressor', 'Support Vector Regressor'])
@@ -44,7 +44,7 @@ if st.button('Predecir el precio de la casa'):
         result = predict(data, 'dt_model.sav')
     elif model == 'Support Vector Regressor':
         result = predict(data, 'sv_model.sav')
-            
+          
     st.text(f'El precio de la casa es de: ${result[0]}')
 
     
--- prediction.py
+++ prediction.py
@@ -1,5 +1,8 @@
 import joblib
+from combined_attributes_adder import CombinedAttributesAdder
 
 def predict(data, model_name):
     model = joblib.load(f'models/{model_name}')
-    return model.predict(data)
+    pipeline = joblib.load('models/pipeline.sav')
+    transformed_data = pipeline.transform(data)
+    return model.predict(transformed_data)
